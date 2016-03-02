cd17dd27908013e4a6e2d498766ae3840a3b4733
JoelPub <liangwang_1980@hotmail.com>
2016-03-02 09:29:19 +0800

    $http.then in webAPI
    $http.then in webAPI
    input(option) make group by using name

--- src/themes/lincoln-financial-calc/js/controller/modelSeries.js
+++ src/themes/lincoln-financial-calc/js/controller/modelSeries.js
@@ -1,4 +1,13 @@
-financialCalculator.controller('modelSeriesController',['$scope','routeConstants','appValue',function($scope,routeConstants,appValue){
+financialCalculator.controller('modelSeriesController',['$scope','routeConstants','appValue','$http','webAPI',function($scope,routeConstants,appValue,$http,webAPI){
 	$scope.appValue=appValue;
 	appValue.currentStep=0;
+	// $http.get('../rest/financialcalc/models.json').then(function(data){
+	// 	appValue.model=data;
+	// });
+	webAPI.getModels();
+	$scope.selectModel = function(model) {
+		appValue.selectedModel = model;
+		webAPI.getSeries(model);
+	}
+	
 }])
--- src/themes/lincoln-financial-calc/js/service/webAPI.js
+++ src/themes/lincoln-financial-calc/js/service/webAPI.js
@@ -4,10 +4,22 @@ financialCalculator.factory('webAPI',['$http','$q','appValue',function($http,$q,
 	},
 		services = {
 			get:function(url,appValueAttrName) {
-				if(services.isCached(url)) {
-					
-
-				}
+				return $http.get(url).then(function(data){
+					appValue[appValueAttrName] = data.data;
+					return data.data;
+				},function(error){
+					console.log(error.statusText);
+				});
 			}
-		}
-}])
+		},
+		publicServices = {
+			getModels: function() {
+				return services.get('../rest/financialcalc/models.json','models');
+			},
+			getSeries: function(model){
+				return services.get('../rest/financialcalc/series.json','series');
+			}
+		};
+
+		return publicServices;
+}]);
